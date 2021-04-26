762d54c050655ea688de03a46c960a58c27f7db4
RafaelN9 <73660960+RafaelN9@users.noreply.github.com>
2021-04-26 20:42:55 -0300

    base para barra lateral

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log("Hello World!")
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    if(window.screen.width > 600){
+        document.getElementById("sidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        setTimeout(function(){ 
+            document.getElementById("main").style.width = "calc(100% - 250px)";
+        }, 450);
+    }else{
+        console.log("Tela de dispositivo mobile!");
+        document.getElementById("sidenav").style.width = "100%";
+    }
+   
+    
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    if(window.screen.width < 600 && document.getElementById("sidenav").style.width == "100%"){
+        console.log("Tela de dispositivo mobile!");
+        document.getElementById("sidenav").style.width = "0";
+    }else{
+        document.getElementById("main").style.marginLeft = "0";
+        document.getElementById("sidenav").style.width = "0";
+        document.getElementById("main").style.width = "100%"
+    }
+} 
