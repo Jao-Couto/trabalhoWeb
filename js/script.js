5eae7d41ba1e358db532d3956e90e68f36e7656e
RafaelN9 <73660960+RafaelN9@users.noreply.github.com>
2021-04-28 17:39:07 -0300

    Inicial

--- js/script.js
+++ js/script.js
@@ -1,29 +1,49 @@
 console.log("Hello World!")
 console.log("Hello World!")
 
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+let opened = false
+
 function openNav() {
+    
     if(window.screen.width > 600){
         document.getElementById("sidenav").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-            document.getElementById("main").style.width = "calc(100% - 250px)";
-
+        document.getElementById("main").animate([
+            // keyframes
+            { opacity: '100%' },
+            { opacity: '50%', transform: blur(200)}
+          ], {
+            // timing options
+            duration: 300,
+            fill: "forwards"
+          })
     }else{
         console.log("Tela de dispositivo mobile!");
         document.getElementById("sidenav").style.width = "100%";
     }
-   
-    
+    setTimeout(function(){
+        opened = true
+    }, 500)
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-    if(window.screen.width < 600 && document.getElementById("sidenav").style.width == "100%"){
-        console.log("Tela de dispositivo mobile!");
-        document.getElementById("sidenav").style.width = "0";
-    }else{
-        document.getElementById("main").style.marginLeft = "0";
-        document.getElementById("sidenav").style.width = "0";
-        document.getElementById("main").style.width = "100%"
-    }
+    opened = false
+    document.getElementById("sidenav").style.width = "0";
+    if( document.getElementById("sidenav").style.width != "100%" )
+        document.getElementById("main").animate([
+            // keyframes
+            { opacity: '50%', transform: blur(2) },
+            { opacity: '100%', transform: blur(0) }
+        ], {
+            // timing options
+            duration: 300,
+            fill: "forwards"
+        })
 } 
+
+document.body.addEventListener("click", function(e){
+    console.log(opened);
+    console.log(e.target.id != "sidenav");
+    console.log(e.target.parentElement.id != "sidenav");
+    if (opened && e.target.id != "sidenav" && e.target.parentElement.id != "sidenav")
+        closeNav()
+})
