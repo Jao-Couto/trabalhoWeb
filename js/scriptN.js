5dbf13f094c285945f8fc5bcf4918758c368a689
Jao-Couto <73503848+Jao-Couto@users.noreply.github.com>
2021-05-27 20:01:15 -0300

    Revert "Revert "Pesquisa de Categoria""
    This reverts commit 0d036679eb610409b43d7281024f2bfac2d99cb5.

--- js/scriptN.js
+++ js/scriptN.js
@@ -1,8 +1,13 @@
 let dados_selecionados = [];
 
 function addCarrinho(coordProd){
-    dados_selecionados.push(produtos[parseInt(coordProd)]);
-    sessionStorage.setItem('produtos', JSON.stringify(dados_selecionados));
+    produtos.forEach(produto =>{
+        if(produto.codigo == coordProd){
+            dados_selecionados.push(produto);
+            sessionStorage.setItem('produtos', JSON.stringify(dados_selecionados));
+        }
+    })
+    
 }
 
 function toggleDesc(obj) {
@@ -14,18 +19,23 @@ $(document).ready(function(){
     let maxAmountPerPage = 18;
     let MAX = produtos.length
     let maxPages = MAX/maxAmountPerPage
-    let elementos = createDiv();
+    let elementos = createDiv(produtos);
     let presentPage = 1
     let active = 0
     let activeElm
+    let arrCategoria = []
     inicializaPag()
 
-    $("#itens")[0].innerHTML = ""
-    elementos.slice(0, maxAmountPerPage).forEach(elemento => {
-        $("#itens")[0].innerHTML += elemento   
-    }); 
+
+    function carregaPag() {
+        $("#itens")[0].innerHTML = ""
+        elementos.slice(0, maxAmountPerPage).forEach(elemento => {
+            $("#itens")[0].innerHTML += elemento   
+        });    
+    }
     
     function inicializaPag() {
+        carregaPag()
         $("#pageSelect")[0].innerHTML += `<li class="page-item"><a class="page-link" href="#" id="prevBtn">Previous</a></li>`
         for (let i = 0; i < maxPages; i++) {
             $("#pageSelect")[0].innerHTML += `<li class="page-item"><a class="page-link" id="pag`+(i+1)+`">`+(i+1)+`</a></li>`
@@ -33,30 +43,35 @@ $(document).ready(function(){
         $("#pageSelect")[0].innerHTML += `<li class="page-item"><a class="page-link" href="#" id="nextBtn">Next</a></li>`
         activeElm = $("#pageSelect")[0].childNodes[(2+active)]
         activeElm.className += " active"
-        console.log($("#pageSelect")[0].childNodes[(2+active)]);  
-    }
 
+        
+        produtos.forEach(elem => {
+            if(arrCategoria.indexOf(elem.categoria) == -1)
+                arrCategoria.push(elem.categoria)
+        })
+        for (let i = 0; i < arrCategoria.length; i++) {
+            let opt = document.createElement("option")
+            opt.value = arrCategoria[i];
+            opt.innerHTML = arrCategoria[i];
+            $("#dropSelect")[0].appendChild(opt)   
+        }
+    }
     
-    function createDiv(){
+    function createDiv(produtos){
         let respArr = []
-        let nome, preco, desc, img, strResp, i=0
+        let  strResp, i=0
         produtos.forEach(elem =>{
             if(elem.preco != undefined){
-                strResp = ""
-                nome = elem.nome
-                preco = elem.preco.toFixed(2)
-                desc = elem.desc
-                img = elem.img
-                strResp +=   `<div class="col-sm-12 col-lg-3 col-md-4 m-2"><div class="card h-100">
-                                    <img src="`+ img +`" class="card-img-top">
+                strResp =   `<div class="col-sm-12 col-lg-3 col-md-4 m-2"><div class="card h-100">
+                                    <img src="`+ elem.img +`" class="card-img-top">
                                     <div class="card-body">
-                                        <h4 class="card-title">`+ nome +`</h4>
-                                        <p class="card-text text-muted"> R$ `+ preco +`</p>
-                                        <p class="card-text" style="display: none;" id="desc`+i+`">`+ desc +`</p>
+                                        <h4 class="card-title">`+ elem.nome +`</h4>
+                                        <p class="card-text text-muted"> R$ `+ elem.preco.toFixed(2) +`</p>
+                                        <p class="card-text" style="display: none;" id="desc`+i+`">`+ elem.desc +`</p>
                                         <button type="button" class="btn align-self-end btn-outline-info"  onclick="toggleDesc('#desc`+i+`')">Info</button>
                                     </div>
                                     <div class="card-footer bg-dark" style="transform: rotate(0);">
-                                        <a class="stretched-link btn text-white" id='compra`+i+`' onclick="addCarrinho(`+i+`)" name='botao' style="width: 100%;"><h6 class="small">Adicionar ao Carrinho</h6></a>
+                                        <a class="stretched-link btn text-white" id='compra`+i+`' onclick="addCarrinho(`+elem.codigo+`)" name='botao' style="width: 100%;"><h6 class="small">Adicionar ao Carrinho</h6></a>
                                     </div>
                                 </div>
                             </div>`
@@ -66,6 +81,19 @@ $(document).ready(function(){
         })
         return respArr
     }
+
+    $("#btnDropSelect").click(function(){
+        let sel = $("#dropSelect")[0].value
+        if(arrCategoria.indexOf(sel) != -1){
+            let filCategoria = []
+            produtos.forEach(prod =>{
+                if(prod.categoria == sel)
+                    filCategoria.push(prod)
+            })
+            elementos = createDiv(filCategoria)
+            carregaPag()
+        }
+    })
 
     $("#nextBtn").click(function(event){
         if(!nextPage()){
@@ -83,8 +111,7 @@ $(document).ready(function(){
         if(presentPage < maxPages){
             presentPage++;
             activeElm.classList.toggle("active");
-            active++
-            activeElm = $("#pageSelect")[0].childNodes[active+2]
+            activeElm = $("#pageSelect")[0].childNodes[presentPage+2]
             activeElm.classList.toggle("active");
             return true
         }
@@ -95,8 +122,7 @@ $(document).ready(function(){
         if(presentPage > 1){
             presentPage--;
             activeElm.classList.toggle("active");
-            active--
-            activeElm = $("#pageSelect")[0].childNodes[active+2]
+            activeElm = $("#pageSelect")[0].childNodes[presentPage+2]
             activeElm.classList.toggle("active");
             return true
         }
